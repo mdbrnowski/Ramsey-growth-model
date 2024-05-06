db2618aebe2bccea6f45cb053adab4d0e8dfceb0
Michał Dobranowski <mdbrnowski@gmail.com>
2024-05-06 10:43:05 +0200

    Create solution for finite time case

--- src/RamseyGrowthModel.jl
+++ src/RamseyGrowthModel.jl
@@ -2,8 +2,9 @@ module RamseyGrowthModel
 
 export GrowthModel
 
-using Roots
 using Enzyme
+using DataFrames
+using Roots
 
 include("sample_functions.jl")
 
@@ -22,14 +23,52 @@ GrowthModel(
     A::Float64   # technology
 ) = GrowthModel(β, δ, sample_u(γ), sample_f(A, α))
 
-function next_k_c(model::GrowthModel, k, c)::Tuple{Float64, Float64}
-    u′(c::Float64) = autodiff(Reverse, model.u, Active, Active(c))[1][1]
-    f′(K_t::Float64) = autodiff(Reverse, model.f, Active, Active(K_t))[1][1]
 
-    next_k = model.f(k) + (1 - model.δ) * k - c
-    next_k >= 0 || error("Capital k is negative!")
-    next_c = find_zero(x -> u′(x) - u′(c) / (model.β * (f′(next_k) + 1 - model.δ)), (0, Inf64))
-    next_k, next_c
+function next_K_C(model::GrowthModel, K, C)::Tuple{Float64,Float64}
+    u′(c::Float64) = autodiff(Reverse, model.u, Active, Active(c))[1][1]
+    f′(k::Float64) = autodiff(Reverse, model.f, Active, Active(k))[1][1]
+
+    next_K = model.f(K) + (1 - model.δ) * K - C
+    next_K >= 0 || return NaN, NaN
+    next_C = find_zero(x -> u′(x) - u′(C) / (model.β * (f′(next_K) + 1 - model.δ)), (0, Inf64))
+    next_K, next_C
+end
+
+function shooting(model::GrowthModel, T::Int64, K₀::Float64, C₀::Float64)::DataFrame
+    allocation = DataFrame(
+        t=0:T,
+        K=fill(NaN, T + 1),
+        C=fill(NaN, T + 1)
+    )
+    allocation[1, "K"] = K₀
+    allocation[1, "C"] = C₀
+    for i ∈ 1:T
+        Kₜ, Cₜ = next_K_C(model, allocation[i, "K"], allocation[i, "C"])
+        if isnan(Cₜ)
+            break
+        end
+        allocation[i+1, "K"], allocation[i+1, "C"] = Kₜ, Cₜ
+    end
+    allocation
+end
+
+function find_best_allocation(model::GrowthModel, T::Int64, K₀::Float64; tol::Float64=K₀/1e6)::DataFrame
+    C_low, C_high = 0, model.f(K₀)
+
+    # todo: add a loop failure exit after too many iterations
+    # todo: log number of iterations before return
+    while true
+        C_mid = (C_low + C_high) / 2
+        allocation = shooting(model, T, K₀, C_mid)
+        next_K = next_K_C(model, last(allocation).K, last(allocation).C)[1]
+        if isnan(next_K)
+            C_high = C_mid
+        elseif next_K > tol
+            C_low = C_mid
+        else
+            return allocation
+        end
+    end
 end
 
 end # module RamseyGrowthModel
